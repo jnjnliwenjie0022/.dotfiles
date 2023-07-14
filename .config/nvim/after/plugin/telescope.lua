f9cf4c7fd796a2179ad553e7c3a2a33cdd26e6b4
wen-jie li <jasonli@andestech.com>
2023-07-14 10:41:46 +0800

    update telescope.lua for search string

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -16,8 +16,8 @@ telescope.setup {
 }
 
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>f', builtin.find_files, {}) 
+local opts = { noremap = true, silent = true }
+vim.keymap.set('n', '<leader>f', builtin.find_files, opts) 
+vim.keymap.set('n', '<leader>/', builtin.live_grep, opts)
+
 --vim.keymap.set('n', '<C-p>', builtin.git_files, {}) 
-vim.keymap.set('n', '<leader>/', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
