0af244f41c4ec1264edca378db65cc28e5452cad
jnjn0022 <jnjn0022@gmail.com>
2023-07-19 02:45:15 +0800

    update harpoon.lua and tmux-sessionizer

--- .config/nvim/after/plugin/harpoon.lua
+++ .config/nvim/after/plugin/harpoon.lua
@@ -7,8 +7,8 @@ end
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-vim.keymap.set("n", "<leader>v", mark.add_file)
-vim.keymap.set("n", "<leader>b", ui.toggle_quick_menu)
+vim.keymap.set("n", "<leader>a", mark.add_file)
+vim.keymap.set("n", "<leader>m", ui.toggle_quick_menu)
 vim.keymap.set("n", "<leader>n", ui.nav_next)
 --vim.keymap.set("n", "<C-e>", Telescope harpoon marks<CR>)
 
