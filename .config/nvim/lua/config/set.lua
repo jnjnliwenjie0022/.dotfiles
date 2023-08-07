eea71ade9be0603a6c9fa5d0e1b7b1a786256ba2
jnjn0022 <jnjn0022@gmail.com>
2023-08-07 16:02:16 +0800

    update set.lua

--- .config/nvim/lua/config/set.lua
+++ .config/nvim/lua/config/set.lua
@@ -2,6 +2,12 @@ vim.opt.termguicolors = true
 
 vim.opt.clipboard = "unnamedplus"
 
+vim.opt.updatetime = 50
+
+vim.g['netrw_banner'] = 0
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
 vim.opt.nu = true
 vim.opt.numberwidth=4
 vim.opt.relativenumber = true
@@ -15,21 +21,14 @@ vim.opt.expandtab = true
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
+vim.opt.scrolloff = 8
 
 vim.opt.hlsearch = true 
 vim.opt.incsearch = true
 
 vim.opt.guicursor = "" --always has fat cursor
 
-vim.opt.scrolloff = 8
 --vim.opt.swapfile = false
 --vim.opt.backup = false
 --vim.opt.undodir = os.getenv("HOME") .. "\.vim\undodir"
 --vim.opt.undofile = true
-
-vim.opt.updatetime = 50
-
-vim.g['netrw_banner'] = 0
-
-vim.opt.splitright = true
-vim.opt.splitbelow = true
