84b08a7b77199bb45cb7e516c661553d9af5ac92
wen-jie li <jasonli@andestech.com>
2023-08-07 14:25:36 +0800

    update ctag.lua

--- .config/nvim/lua/config/ctags.lua
+++ .config/nvim/lua/config/ctags.lua
@@ -3,10 +3,10 @@ function run_ctags()
     vim.cmd [[!mkdir $HOME/.local/tags ]]
 
     --https://docs.ctags.io/en/latest/output-format.html
-    vim.cmd [[!ctags -f $HOME/.local/tags/.uvm1.1d.tags --languages=systemverilog --tag-relative=never -R $HOME/synopsys/uvm-1.1d/src ]]
+    vim.cmd [[!ctags -f $HOME/.local/tags/.uvm.tags --languages=systemverilog --tag-relative=never -R $UVM_HOME/src ]]
     vim.cmd [[!ctags -f $HOME/.local/tags/.tags --tag-relative=never --exclude=.git -R .]]
 
-    vim.cmd [[ set tags=$HOME/.local/tags/.uvm1.1d.tags ]]
+    vim.cmd [[ set tags=$HOME/.local/tags/.uvm.tags ]]
     vim.cmd [[ set tags+=$HOME/.local/tags/.tags ]]
 end
 
