7c124628f57c14902035e7669505db0293d1a995
wen-jie li <jasonli@andestech.com>
2023-08-09 11:23:10 +0800

    udpate lsp

--- .config/nvim/lua/config/packer.lua
+++ .config/nvim/lua/config/packer.lua
@@ -43,26 +43,26 @@ return require('packer').startup(function(use)
     -- https://github.com/ludovicchabant/vim-gutentags
     --use { 'ludovicchabant/vim-gutentags' }
 
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v2.x',
-        requires = {
-            -- LSP Support
-            {'neovim/nvim-lspconfig'},
-            {'williamboman/mason.nvim'},
-            {'williamboman/mason-lspconfig.nvim'},
+    --use {
+    --    'VonHeikemen/lsp-zero.nvim',
+    --    branch = 'v2.x',
+    --    requires = {
+    --        -- LSP Support
+    --        {'neovim/nvim-lspconfig'},
+    --        {'williamboman/mason.nvim'},
+    --        {'williamboman/mason-lspconfig.nvim'},
 
-            -- Autocompletion
-            {'hrsh7th/nvim-cmp'},
-            {'hrsh7th/cmp-buffer'},
-            {'hrsh7th/cmp-path'},
-            {'saadparwaiz1/cmp_luasnip'},
-            {'hrsh7th/cmp-nvim-lsp'},
-            {'hrsh7th/cmp-nvim-lua'},
+    --        -- Autocompletion
+    --        {'hrsh7th/nvim-cmp'},
+    --        {'hrsh7th/cmp-buffer'},
+    --        {'hrsh7th/cmp-path'},
+    --        {'saadparwaiz1/cmp_luasnip'},
+    --        {'hrsh7th/cmp-nvim-lsp'},
+    --        {'hrsh7th/cmp-nvim-lua'},
 
-            -- Snippets
-            {'L3MON4D3/LuaSnip'},
-            {'rafamadriz/friendly-snippets'},
-        }
-    }
+    --        -- Snippets
+    --        {'L3MON4D3/LuaSnip'},
+    --        {'rafamadriz/friendly-snippets'},
+    --    }
+    --}
 end)
