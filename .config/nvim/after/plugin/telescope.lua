adfe90e9059ee27046e3bc89a1601f148ade1b2f
jnjn0022 <jnjn0022@gmail.com>
2023-08-10 01:58:11 +0800

    udpate telescope for tag and quickfix list

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -4,19 +4,67 @@ if (not status) then
     return
 end
 
+--keymaps: $HOME/.local/share/nvim/site/pack/packer/start/telescope.nvim/lua/telescope/mappings.lua
 local actions = require('telescope.actions')
 telescope.setup {
     defaults = {
         mappings = {
             n = {
-                ['q'] = actions.close
+                ['q'] = actions.close,
             },
-        }
-    }
+        },
+    },
+    pickers = {
+        tags = {
+            only_sort_tags = true,
+            fname_width = 100
+        },
+    },
 }
 
 local builtin = require('telescope.builtin')
 local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<leader>f', builtin.find_files, opts) 
-vim.keymap.set('n', '<leader>/', builtin.live_grep, opts)
-vim.keymap.set('n', '<leader>g', builtin.git_files, opts) 
+vim.keymap.set('n', '<leader>f', function()
+    builtin.find_files({
+        layout_strategy = "vertical",
+        layout_config = {
+            width = 0.9,
+            height = 0.9,
+        },
+        hidden = true,
+        previewer = false,
+    })
+end, opts)
+vim.keymap.set('n', '<leader>g', function()
+    builtin.git_files({
+        layout_strategy = "vertical",
+        layout_config = {
+            width = 0.9,
+            height = 0.9,
+        },
+        hidden = true,
+        previewer = false,
+    })
+end, opts)
+vim.keymap.set('n', '<leader>/', function()
+    builtin.live_grep({
+        layout_strategy = "vertical",
+        layout_config = {
+            width = 0.9,
+            height = 0.9,
+        },
+        hidden = false,
+        previewer = true,
+    })
+end, opts)
+vim.keymap.set('n', '<leader>t', function()
+    builtin.tags({
+        layout_strategy = "vertical",
+        layout_config = {
+            width = 0.9,
+            height = 0.9,
+        },
+        hidden = false,
+        previewer = true,
+    })
+end, opts)
--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,6 +2,7 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>e", "<CMD>Ex<CR>")
 
+vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-a>", "<nop>") -- avoid comflict with tmux
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
@@ -14,14 +15,16 @@ vim.keymap.set("v", "p", "\"_dP")
 
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<C-o>", "<C-o>zz")
 vim.keymap.set("n", "<C-i>", "<C-i>zz")
 
+vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>zz")
+
 --https://gist.github.com/seanh/a866462a27cb3ad7b084c8e6000a06b9
---vim.keymap.set("n", "<C-n>",     "<cmd>cnext<CR>zz")
---vim.keymap.set("n", "<C-N>",     "<cmd>cprev<CR>zz")
 --vim.keymap.set("n", "<leader>n", "<cmd>lnext<CR>zz")
 --vim.keymap.set("n", "<leader>N", "<cmd>lprev<CR>zz")
 
