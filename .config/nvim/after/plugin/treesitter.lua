a6455de1e8f78584f9d9ad331ece66629d990abd
jnjn0022 <jnjn0022@gmail.com>
2023-08-10 13:27:34 +0800

    update treesitter.lua

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,14 +1,14 @@
 require'nvim-treesitter.configs'.setup {
-  --ensure_installed = { "c", "lua", "bash" },
-  sync_install = false,
-  auto_install = false,
-  highlight = {
-    enable = true,
+    --ensure_installed = { "c", "lua", "bash" },
+    sync_install = false,
+    auto_install = false,
+    highlight = {
+        enable = true,
 
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = false,
+    },
 }
