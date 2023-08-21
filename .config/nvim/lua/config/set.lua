9a189784f70f9f64733453059cdbb714a013cdd1
jnjn0022 <jnjn0022@gmail.com>
2023-08-21 13:18:49 +0800

    update clipboard set.lua

--- .config/nvim/lua/config/set.lua
+++ .config/nvim/lua/config/set.lua
@@ -64,4 +64,15 @@ vim.opt.clipboard = "unnamedplus"
 --            \   'cache_enabled': 0,
 --    \ }
 --]]
---
+vim.g.clipboard = {
+ name = "xsel",
+ copy = {
+  ["+"] = "xsel --nodetach -i -b",
+  ["*"] = "xsel --nodetach -i -p",
+ },
+ paste = {
+  ["+"] = "xsel  -o -b",
+  ["*"] = "xsel  -o -b",
+ },
+ cache_enabled = 1,
+}
