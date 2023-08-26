402244f51beda9d6d9c91f1032e2db84e236309a
jnjn0022 <jnjn0022@gmail.com>
2023-08-27 03:55:43 +0800

    udpate

--- .config/nvim/lua/config/set.lua
+++ .config/nvim/lua/config/set.lua
@@ -7,7 +7,6 @@ vim.g.python_host_prog='/bin/python2'
 vim.g.python3_host_skip_check = 1
 vim.g.python3_host_prog='/bin/python3'
 vim.opt.pyxversion=3
-vim.g.EditorConfig_core_mode = 'external_command'
 -- https://vi.stackexchange.com/a/5318/7339
 vim.g.matchparen_timeout = 20
 vim.g.matchparen_insert_timeout = 20
