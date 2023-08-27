104c8b2cb5cd21ffccf17cfbeb0491cd4fec7b61
jnjn0022 <jnjn0022@gmail.com>
2023-08-27 14:55:13 +0800

    update

--- .config/nvim/lua/config/init.lua
+++ .config/nvim/lua/config/init.lua
@@ -1,3 +1,4 @@
 require("config.set")
+require("config.autocmd")
 require("config.keymaps")
 require("config.ctags")
--- .config/nvim/lua/config/set.lua
+++ .config/nvim/lua/config/set.lua
@@ -1,5 +1,8 @@
 -- IMPROVE NEOVIM STARTUP
 -- https://github.com/editorconfig/editorconfig-vim/issues/50
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
 vim.g.loaded_python_provier = 0
 vim.g.loaded_python3_provider = 0
 vim.g.python_host_skip_check = 1
@@ -43,11 +46,6 @@ for _, plugin in pairs(disabled_built_ins) do
     vim.g["loaded_" .. plugin] = 1
 end
 
-vim.g.loaded_node_provider = 0
-vim.g.loaded_perl_provider = 0
-vim.g.loaded_ruby_provider = 0
-vim.g.loaded_python3_provider = 0
-
 vim.opt.termguicolors = true
 vim.opt.updatetime = 50
 
@@ -82,53 +80,3 @@ vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undofile = true
 --vim.opt.undodir = os.getenv("HOME") .. "\.vim\undodir"
-
-vim.cmd [[
-    augroup preWrite
-        autocmd!
-        autocmd BufWritePre,FileWritePre * :%s/\s\+$//e | %s/\r$//e
-    augroup END
-]]
-
-vim.cmd [[
-    autocmd TermOpen * :setlocal nonumber norelativenumber
-]]
-
---" 快速匹配Todo
---command TODO :call FindFile1()
---func FindFile1()
---  " 可以根据自己的开发习惯来设置管理path， 从当前项目的根目录匹配
---  let a = substitute(matchstr(expand("%:p"), 'dev/\(.*\)/'), '/src.*', '', 'g')
---  execute ':vimgrep /TODO/gj ~/Documents/'.a.'/src/** |copen'
---endfunc
---
---" 高亮显示
---hi Todo term=bold ctermfg=0 ctermbg=15 gui=none guifg=none
-
---vim.opt.clipboard = "unnamedplus"
---vim.cmd [[
---    let g:clipboard = {
---            \   'name': 'local bin xsel',
---            \   'copy': {
---            \      '+': $HOME.'/.local/bin/xsel -i -b',
---            \      '*': $HOME.'/.local/bin/xsel -i -p',
---            \    },
---            \   'paste': {
---            \      '+': $HOME.'/.local/bin/xsel -o -b',
---            \      '*': $HOME.'/.local/bin/xsel -o -p',
---            \   },
---            \   'cache_enabled': 0,
---    \ }
---]]
---vim.g.clipboard = {
--- name = "xsel",
--- copy = {
---  ["+"] = "xsel -i -b",
---  ["*"] = "xsel -i -p",
--- },
--- paste = {
---  ["+"] = "xsel  -o -b",
---  ["*"] = "xsel  -o -p",
--- },
--- cache_enabled = 0,
---}
