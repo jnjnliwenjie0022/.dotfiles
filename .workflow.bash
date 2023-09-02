a2d6bdd2e40f1fe0295c16b91480948ae9e6d887
jnjn0022 <jnjn0022@gmail.com>
2023-09-02 22:26:03 +0800

    update alacritty.yml

--- .workflow.bash
+++ .workflow.bash
@@ -1,6 +1,10 @@
+# wsl --install Ubuntu
+# wsl --unregister Ubuntu
+#
 # terminal color scheme config
 # https://sourcegraph.com/github.com/mbadolato/iTerm2-Color-Schemes/-/blob/mobaxterm/catppuccin-mocha.ini
 #
+#{{{ MobaXterm
 # Paste the following configurations in the corresponding place in MobaXterm.ini.
 # Setting->Configuration->Terminal->Default terminal color setting->import catppuccin-mocha.ini
 #
@@ -26,6 +30,95 @@
 # White=186,194,222
 # BoldBlack=88,91,112
 # BoldRed=243,139,168
+#}}}
+
+#{{{ Alacritty
+#
+# C:\Windows\System32\wsl.exe -d Ubuntu
+# C:\Windows\System32\wsl.exe -d Debian
+#
+# config
+# $env:APPDATA\alacritty\alacritty.yml
+#
+# colors:
+#     primary:
+#         background: "#000000" # base
+#         foreground: "#CDD6F4" # text
+#         # Bright and dim foreground colors
+#         dim_foreground: "#CDD6F4" # text
+#         bright_foreground: "#CDD6F4" # text
+#
+#     # Cursor colors
+#     cursor:
+#         text: "#1E1E2E" # base
+#         cursor: "#F5E0DC" # rosewater
+#     vi_mode_cursor:
+#         text: "#1E1E2E" # base
+#         cursor: "#B4BEFE" # lavender
+#
+#     # Search colors
+#     search:
+#         matches:
+#             foreground: "#1E1E2E" # base
+#             background: "#A6ADC8" # subtext0
+#         focused_match:
+#             foreground: "#1E1E2E" # base
+#             background: "#A6E3A1" # green
+#         footer_bar:
+#             foreground: "#1E1E2E" # base
+#             background: "#A6ADC8" # subtext0
+#
+#     # Keyboard regex hints
+#     hints:
+#         start:
+#             foreground: "#1E1E2E" # base
+#             background: "#F9E2AF" # yellow
+#         end:
+#             foreground: "#1E1E2E" # base
+#             background: "#A6ADC8" # subtext0
+#
+#     # Selection colors
+#     selection:
+#         text: "#1E1E2E" # base
+#         background: "#F5E0DC" # rosewater
+#
+#     # Normal colors
+#     normal:
+#         black: "#45475A" # surface1
+#         red: "#F38BA8" # red
+#         green: "#A6E3A1" # green
+#         yellow: "#F9E2AF" # yellow
+#         blue: "#89B4FA" # blue
+#         magenta: "#F5C2E7" # pink
+#         cyan: "#94E2D5" # teal
+#         white: "#BAC2DE" # subtext1
+#
+#     # Bright colors
+#     bright:
+#         black: "#585B70" # surface2
+#         red: "#F38BA8" # red
+#         green: "#A6E3A1" # green
+#         yellow: "#F9E2AF" # yellow
+#         blue: "#89B4FA" # blue
+#         magenta: "#F5C2E7" # pink
+#         cyan: "#94E2D5" # teal
+#         white: "#A6ADC8" # subtext0
+#
+#     # Dim colors
+#     dim:
+#         black: "#45475A" # surface1
+#         red: "#F38BA8" # red
+#         green: "#A6E3A1" # green
+#         yellow: "#F9E2AF" # yellow
+#         blue: "#89B4FA" # blue
+#         magenta: "#F5C2E7" # pink
+#         cyan: "#94E2D5" # teal
+#         white: "#BAC2DE" # subtext1
+#
+#     indexed_colors:
+#         - { index: 16, color: "#FAB387" }
+#         - { index: 17, color: "#F5E0DC" }
+#}}}
 
 echo "Shell=$SHELL $BASH_VERSION"
 # language config
