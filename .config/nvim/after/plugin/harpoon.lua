4ec73039376d474c82dc6c06af52dafd95e75a95
wen-jie li <jasonli@andestech.com>
2023-09-06 13:38:55 +0800

    update

--- .config/nvim/after/plugin/harpoon.lua
+++ .config/nvim/after/plugin/harpoon.lua
@@ -9,10 +9,11 @@ end)
 
 vim.keymap.set("n", "<leader>m", ui.toggle_quick_menu)
 vim.keymap.set("n", "<leader>n", ui.nav_next)
-vim.keymap.set("n", "<leader>j", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<leader>k", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<leader>l", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<leader>;", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<leader>h", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<leader>j", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<leader>k", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<leader>l", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<leader>;", function() ui.nav_file(5) end)
 
 require('harpoon').setup({
     tabline = true,
