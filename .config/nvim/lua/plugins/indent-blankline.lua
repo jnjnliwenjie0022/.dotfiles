eedf04e16dfe2681b53bd31b0ca237c8b23b7fd4
jnjn0022 <jnjn0022@gmail.com>
2023-09-09 13:49:17 +0800

    update

---
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -0,0 +1,12 @@
+return {
+    "lukas-reineke/indent-blankline.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function ()
+        vim.opt.list = true
+
+        require("indent_blankline").setup {
+            space_char_blankline = " ",
+            show_current_context = true,
+        }
+    end
+}
