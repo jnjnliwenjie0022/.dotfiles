47edd4d4ae51d8bfdca5ad545cfc402c9a5032c8
wen-jie li <jasonli@andestech.com>
2023-10-17 14:00:15 +0800

    update

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -8,13 +8,25 @@ return {
         local mark = require("harpoon.mark")
         local ui = require("harpoon.ui")
 
+        vim.cmd [[ hi HarpoonNumberActive   guifg=#000000 guibg=#d7ff00 ]]
+        vim.cmd [[ hi HarpoonActive         guifg=#000000 guibg=#d7ff00 ]]
+        vim.cmd [[ hi HarpoonInactive       guifg=#cdd6f4 guibg=#262626 ]]
+        vim.cmd [[ hi HarpoonNumberInactive guifg=#cdd6f4 guibg=#262626 ]]
+        vim.cmd [[ hi TabLineFill           guifg=#cdd6f4 guibg=#262626 ]]
+
+        require('harpoon').setup({
+            tabline = true,
+            menu = {
+                width = vim.api.nvim_win_get_width(0) - 22,
+            }
+        })
+
         vim.keymap.set("n", "<leader>,", function()
             mark.add_file()
             require('harpoon').setup({
                 tabline = true,
             })
         end)
-
         vim.keymap.set("n", "<leader>m", ui.toggle_quick_menu)
         vim.keymap.set("n", "<leader>n", ui.nav_next)
         vim.keymap.set("n", "<leader>h", function() ui.nav_file(1) end)
@@ -23,14 +35,5 @@ return {
         vim.keymap.set("n", "<leader>l", function() ui.nav_file(4) end)
         vim.keymap.set("n", "<leader>;", function() ui.nav_file(5) end)
 
-        require('harpoon').setup({
-            tabline = true,
-        })
-
-        vim.cmd [[ hi HarpoonNumberActive   guifg=#000000 guibg=#d7ff00 ]]
-        vim.cmd [[ hi HarpoonActive         guifg=#000000 guibg=#d7ff00 ]]
-        vim.cmd [[ hi HarpoonInactive       guifg=#cdd6f4 guibg=#262626 ]]
-        vim.cmd [[ hi HarpoonNumberInactive guifg=#cdd6f4 guibg=#262626 ]]
-        vim.cmd [[ hi TabLineFill           guifg=#cdd6f4 guibg=#262626 ]]
     end
 }
