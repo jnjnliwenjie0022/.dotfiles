173b669ac86211620db8630d9edbbe2d2d3956dd
wen-jie li <jasonli@andestech.com>
2024-03-22 22:54:28 +0800

    update

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -8,7 +8,6 @@ return {
         local mark = require("harpoon.mark")
         local ui = require("harpoon.ui")
 
-
         require('harpoon').setup({
             tabline = false,
             menu = {
@@ -24,28 +23,23 @@ return {
 
         vim.keymap.set("n", "<leader>,", function()
             local file_path = vim.fn.expand('%:p')
-            mark.add_file()
             print("Hook on harpoon:", file_path)
+            mark.add_file()
+        end)
+        vim.keymap.set("n", "<leader>m", function()
             require('harpoon').setup({
                 tabline = false,
+                menu = {
+                    width = vim.api.nvim_win_get_width(0) - 22,
+                }
             })
+            ui.toggle_quick_menu()
         end)
-        vim.keymap.set("n", "<leader>m", function() ui.toggle_quick_menu() end)
         vim.keymap.set("n", "<leader>n", function() ui.nav_next() end)
         vim.keymap.set("n", "<leader>h", function() ui.nav_file(1) end)
         vim.keymap.set("n", "<leader>j", function() ui.nav_file(2) end)
         vim.keymap.set("n", "<leader>k", function() ui.nav_file(3) end)
         vim.keymap.set("n", "<leader>l", function() ui.nav_file(4) end)
         vim.keymap.set("n", "<leader>;", function() ui.nav_file(5) end)
-
-        ---- vimenter event
-        --vim.api.nvim_create_autocmd({ "VimEnter" }, {
-        --    pattern = "*",
-        --    callback = function()
-        --        ui.toggle_quick_menu()
-        --    end,
-        --})
-
-
     end
 }
