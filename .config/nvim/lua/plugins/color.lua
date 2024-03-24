d5faa1c768268c23953458bfaa87e88837371322
wen-jie li <jasonli@andestech.com>
2024-03-24 21:31:23 +0800

    update

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -42,7 +42,7 @@ return {
             vim.cmd [[ hi DiffDelete   gui=NONE guifg=#000000 guibg=#000000 ]] -- diff mode: Deleted line
             vim.cmd [[ hi DiffChange   gui=NONE               guibg=#5f0000 ]] -- diff mode: Changed line
             vim.cmd [[ hi DiffText     gui=NONE               guibg=#5f0000 ]] -- diff mode: Changed text within a changed line
-            vim.cmd [[ hi MatchParen   gui=NONE guifg=#000000 guibg=#d7ff00 ]] -- syntax pair
+            vim.cmd [[ hi MatchParen   gui=NONE guifg=#000000 guibg=#87ffff ]] -- syntax pair
 
             vim.cmd [[ hi Title        gui=NONE guifg=#000000               ]]
             vim.cmd [[ hi TabLine      gui=NONE guifg=#cdd6f4 guibg=#262626 ]]
