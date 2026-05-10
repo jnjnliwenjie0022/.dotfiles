04251e4988b90b054e3e19cf5b15bfb24e3ffcbc
Wen-Jie Li <jnjn0022@gmail.com>
2026-05-11 01:53:31 +0800

    update

--- .workflow.bash
+++ .workflow.bash
@@ -282,7 +282,7 @@ bind '"\C-se":"tmux-session-selector\n"'
 #{{{ git config
 git() { if [[ $@ == "pull" ]]; then command echo "Instead of pull, using > git fetch --all -p && git rebase"; else command git "$@"; fi; }
 git config --global core.editor "vim"
-#git config --global core.pager "less -S"
+git config --global core.pager "less -S"
 # - ref: https://www.youtube.com/watch?v=aolI_Rz0ZqY&t=905s
 git config --global user.name "Wen-Jie Li"
 git config --global user.email "jnjn0022@gmail.com"
@@ -305,7 +305,7 @@ git config --global alias.root 'rev-parse --show-toplevel'
 # > git ls/ll --author="Wen-Jie"
 # > git ls/ll -- ${filepath}
 git config --global alias.ls "log --decorate --oneline --graph --reflog"
-git config --global alias.ll "log --decorate --oneline --graph --reflog --date=format:%Y-%m-%d\ %H:%M --pretty=tformat:'%C(auto,yellow)%h %C(auto,blue)%ad %C(auto,green)%<(7,trunc)%aN%C(reset)%C(auto)%d%C(reset)%<(70,trunc) %s'"
+git config --global alias.ll "log --decorate --oneline --graph --reflog --date=format:%Y-%m-%d\ %H:%M --pretty=tformat:'%C(auto,yellow)%h %C(auto,blue)%ad %C(auto,green)%<(7,trunc)%aN%C(reset)%C(auto)%d%C(reset) %s'"
 git config --global alias.rl "reflog --pretty=tformat:'%Cred%h%Creset %C(yellow)%gd%C(reset) %C(auto)%gs%C(reset) %C(green)(%cr)%C(reset) %C(bold blue)<%an>%Creset' --abbrev-commit"
 git config --global alias.tree "log --graph --simplify-by-decoration --pretty=tformat:'%C(auto,blue)%cr%C(auto)%d' --all"
 git config --global alias.ft "fetch --all -p"
@@ -574,96 +574,96 @@ fi
 #}}}
 
 
-## - ref: https://github.com/andrew8088/dotfiles/blob/main/zsh/git.zsh
-##!/bin/bash
+# - ref: https://github.com/andrew8088/dotfiles/blob/main/zsh/git.zsh
+#!/bin/bash
+
+# Copied from Gary Bernhardt (destroyallsoftware.com) dot files repository.
 #
-## Copied from Gary Bernhardt (destroyallsoftware.com) dot files repository.
-##
-## Log output:
-##
-## * 51c333e    (12 days)    <Gary Bernhardt>   add vim-eunuch
-##
-## Branch output:
-##
-## * release/v1.1    (13 days)    <Leyan Lo>   add pretty_git_branch
-##
-## The time massaging regexes start with ^[^<]* because that ensures that they
-## only operate before the first "<". That "<" will be the beginning of the
-## author name, ensuring that we don't destroy anything in the commit message
-## that looks like time.
-##
-## The log format uses } characters between each field, and `column` is later
-## used to split on them. A } in the commit subject or any other field will
-## break this.
+# Log output:
 #
-#LOG_HASH="%C(always,yellow)%h%C(always,reset)"
-#LOG_RELATIVE_TIME="%C(always,green)(%ar)%C(always,reset)"
-#LOG_AUTHOR="%C(always,blue)<%an>%C(always,reset)"
-#LOG_REFS="%C(always,red)%d%C(always,reset)"
-#LOG_SUBJECT="%s"
+# * 51c333e    (12 days)    <Gary Bernhardt>   add vim-eunuch
 #
-#LOG_FORMAT="$LOG_HASH}$LOG_AUTHOR}$LOG_RELATIVE_TIME}$LOG_SUBJECT $LOG_REFS"
+# Branch output:
 #
-#BRANCH_PREFIX="%(HEAD)"
-#BRANCH_REF="%(color:red)%(color:bold)%(refname:short)%(color:reset)"
-#BRANCH_HASH="%(color:yellow)%(objectname:short)%(color:reset)"
-#BRANCH_DATE="%(color:green)(%(committerdate:relative))%(color:reset)"
-#BRANCH_AUTHOR="%(color:blue)%(color:bold)<%(authorname)>%(color:reset)"
-#BRANCH_CONTENTS="%(contents:subject)"
+# * release/v1.1    (13 days)    <Leyan Lo>   add pretty_git_branch
 #
-#BRANCH_FORMAT="}$BRANCH_PREFIX}$BRANCH_REF}$BRANCH_HASH}$BRANCH_DATE}$BRANCH_AUTHOR}$BRANCH_CONTENTS"
+# The time massaging regexes start with ^[^<]* because that ensures that they
+# only operate before the first "<". That "<" will be the beginning of the
+# author name, ensuring that we don't destroy anything in the commit message
+# that looks like time.
 #
-#show_git_head() {
-#    pretty_git_log -1
-#    git show -p --pretty="tformat:"
-#}
-#
-#pretty_git_log() {
-#    git log --since="1 months ago" --graph --pretty="tformat:${LOG_FORMAT}" $* | pretty_git_format | git_page_maybe
-#}
-#
-#pretty_git_log_long() {
-#    git log --since="12 months ago" --graph --pretty="tformat:${LOG_FORMAT}" $* | pretty_git_format | git_page_maybe
-#}
-#
-#pretty_git_log_all() {
-#    git log --all --since="12 months ago" --graph --pretty="tformat:${LOG_FORMAT}" $* | pretty_git_format | git_page_maybe
-#}
-#
-#
-#pretty_git_branch() {
-#    git branch -v --color=always --format=${BRANCH_FORMAT} $* | pretty_git_format
-#}
-#
-#pretty_git_branch_sorted() {
-#    git branch -v --color=always --format=${BRANCH_FORMAT} --sort=-committerdate $* | pretty_git_format
-#}
-#
-#pretty_git_format() {
-#    # Replace (2 years ago) with (2 years)
-#    sed -Ee 's/(^[^)]*) ago\)/\1)/' |
-#    # Replace (2 years, 5 months) with (2 years)
-#    sed -Ee 's/(^[^)]*), [[:digit:]]+ .*months?\)/\1)/' |
-#    # Shorten time
-#    sed -Ee 's/ seconds?\)/s\)/' |
-#    sed -Ee 's/ minutes?\)/m\)/' |
-#    sed -Ee 's/ hours?\)/h\)/' |
-#    sed -Ee 's/ days?\)/d\)/' |
-#    sed -Ee 's/ weeks?\)/w\)/' |
-#    sed -Ee 's/ months?\)/M\)/' |
-#    # Shorten names
-#    sed -Ee $'s/\033\\[34m<Andrew Burgess>/\033[31m<me>/' |
-#    sed -Ee 's/<([^ >]+) [^>]*>/<\1>/' |
-#    # Line columns up based on } delimiter
-#    column -s '}' -t
-#}
-#
-#git_page_maybe() {
-#    # Page only if we're asked to.
-#    if [ -n "${GIT_NO_PAGER}" ]; then
-#        cat
-#    else
-#        # Page only if needed.
-#        less --quit-if-one-screen --no-init --RAW-CONTROL-CHARS --chop-long-lines
-#    fi
-#}
+# The log format uses } characters between each field, and `column` is later
+# used to split on them. A } in the commit subject or any other field will
+# break this.
+
+LOG_HASH="%C(always,yellow)%h%C(always,reset)"
+LOG_RELATIVE_TIME="%C(always,green)(%ar)%C(always,reset)"
+LOG_AUTHOR="%C(always,blue)<%an>%C(always,reset)"
+LOG_REFS="%C(always,red)%d%C(always,reset)"
+LOG_SUBJECT="%s"
+
+LOG_FORMAT="$LOG_HASH}$LOG_AUTHOR}$LOG_RELATIVE_TIME}$LOG_SUBJECT $LOG_REFS"
+
+BRANCH_PREFIX="%(HEAD)"
+BRANCH_REF="%(color:red)%(color:bold)%(refname:short)%(color:reset)"
+BRANCH_HASH="%(color:yellow)%(objectname:short)%(color:reset)"
+BRANCH_DATE="%(color:green)(%(committerdate:relative))%(color:reset)"
+BRANCH_AUTHOR="%(color:blue)%(color:bold)<%(authorname)>%(color:reset)"
+BRANCH_CONTENTS="%(contents:subject)"
+
+BRANCH_FORMAT="}$BRANCH_PREFIX}$BRANCH_REF}$BRANCH_HASH}$BRANCH_DATE}$BRANCH_AUTHOR}$BRANCH_CONTENTS"
+
+show_git_head() {
+    pretty_git_log -1
+    git show -p --pretty="tformat:"
+}
+
+pretty_git_log() {
+    git log --since="1 months ago" --graph --pretty="tformat:${LOG_FORMAT}" $* | pretty_git_format | git_page_maybe
+}
+
+pretty_git_log_long() {
+    git log --since="12 months ago" --graph --pretty="tformat:${LOG_FORMAT}" $* | pretty_git_format | git_page_maybe
+}
+
+pretty_git_log_all() {
+    git log --all --since="12 months ago" --graph --pretty="tformat:${LOG_FORMAT}" $* | pretty_git_format | git_page_maybe
+}
+
+
+pretty_git_branch() {
+    git branch -v --color=always --format=${BRANCH_FORMAT} $* | pretty_git_format
+}
+
+pretty_git_branch_sorted() {
+    git branch -v --color=always --format=${BRANCH_FORMAT} --sort=-committerdate $* | pretty_git_format
+}
+
+pretty_git_format() {
+    # Replace (2 years ago) with (2 years)
+    sed -Ee 's/(^[^)]*) ago\)/\1)/' |
+    # Replace (2 years, 5 months) with (2 years)
+    sed -Ee 's/(^[^)]*), [[:digit:]]+ .*months?\)/\1)/' |
+    # Shorten time
+    sed -Ee 's/ seconds?\)/s\)/' |
+    sed -Ee 's/ minutes?\)/m\)/' |
+    sed -Ee 's/ hours?\)/h\)/' |
+    sed -Ee 's/ days?\)/d\)/' |
+    sed -Ee 's/ weeks?\)/w\)/' |
+    sed -Ee 's/ months?\)/M\)/' |
+    # Shorten names
+    sed -Ee $'s/\033\\[34m<Andrew Burgess>/\033[31m<me>/' |
+    sed -Ee 's/<([^ >]+) [^>]*>/<\1>/' |
+    # Line columns up based on } delimiter
+    column -s '}' -t
+}
+
+git_page_maybe() {
+    # Page only if we're asked to.
+    if [ -n "${GIT_NO_PAGER}" ]; then
+        cat
+    else
+        # Page only if needed.
+        less --quit-if-one-screen --no-init --RAW-CONTROL-CHARS --chop-long-lines
+    fi
+}
