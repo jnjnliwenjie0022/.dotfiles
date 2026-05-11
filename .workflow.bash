1d0cfd0bd4a4fcd4002c97ac9bc9c84314a332b4
Wen-Jie Li <jnjn0022@gmail.com>
2026-05-11 14:30:56 +0800

    update

--- .workflow.bash
+++ .workflow.bash
@@ -393,10 +393,29 @@ function parse_git_branch() {
             status="*"
         fi
 
-        echo "[$branch$status]"
+        echo "$branch$status"
     fi
 }
-export PS1="\[\033[33m\][\w] \[\e[91m\]\$(parse_git_branch) \n\[\033[33m\][\j] $ \[\033[0m\]"
+
+RESET='\[\033[0m\]'
+BLACK='\[\033[30m\]'
+RED='\[\033[31m\]'
+GREEN='\[\033[32m\]'
+YELLOW='\[\033[33m\]'
+BLUE='\[\033[34m\]'
+MAGENTA='\[\033[35m\]'
+CYAN='\[\033[36m\]'
+WHITE='\[\033[37m\]'
+BRIGHT_BLACK='\[\033[90m\]'
+BRIGHT_RED='\[\033[91m\]'
+BRIGHT_GREEN='\[\033[92m\]'
+BRIGHT_YELLOW='\[\033[93m\]'
+BRIGHT_BLUE='\[\033[94m\]'
+BRIGHT_MAGENTA='\[\033[95m\]'
+BRIGHT_CYAN='\[\033[96m\]'
+BRIGHT_WHITE='\[\033[97m\]'
+
+export PS1="${BRIGHT_GREEN}[\W] ${BRIGHT_BLUE}git:(${BRIGHT_RED}\$(parse_git_branch)${BRIGHT_BLUE}) \[\033[33m\][\j] $ ${RESET}"
 
 ## bash-git-promt config
 #file="${HOME}/.local/lib/bash-git-prompt/gitprompt.sh"
